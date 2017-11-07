e22216a95e416aa5a5cdda36d82666692479d335
Ben Reichert <ben@tozny.com>
2017-11-06 17:26:17 -0800

    Add corect location redirect host when ECR tries to redirect us to its domain.

--- aws-ecr-proxy/entrypoint.sh
+++ aws-ecr-proxy/entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-sed -e "s#ECRURL#${ECR_URL}#" haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
+sed -e "s#ECRURL#${ECR_URL}#" haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg.1
+sed -e "s#REGISTRYHOST#${REGISTRY_HOST}#" /usr/local/etc/haproxy/haproxy.cfg.1 > /usr/local/etc/haproxy/haproxy.cfg
 
 rsyslogd -f /usr/local/etc/haproxy/rsyslogd.conf
 
